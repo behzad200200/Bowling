6265f46797e3271de95d73f08c003b960a8b87d6
Behzad <B.banikarimi@gmail.com>
2014-03-27 05:32:47 +0430

    Portotype

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -12,41 +12,44 @@ class Bowling
   end
 
   def count()
-     pins = get_pins
+    pins = get_pins
     frames = Array.new(10) { |i| Frame.new(i+1) }
-     skip_flag = false
+    skip_flag = false
     pins.each_with_index do |pin , index|
       if skip_flag == true
         skip_flag = false
         next
       end
       frame = frames.find {|f| !f.finish?}
-          if index > 9
-            break
-          end
-          if pin == 10
-            if  pins[index.succ].nil?
-              frame.score = pin
-            elsif pins[index.succ.succ].nil?
-              frame.score = pin + pins[index.succ]
-            else
-              frame.score = pin + pins[index.succ]+ pins[index.succ.succ]
-            end
-          elsif  !pins[index.succ].nil? and  pin + pins[index.succ] == 10
-            if pins[index.succ.succ].nil?
-              frame.score = pin + pins[index.succ]
-              skip_flag  = true
-            else
-              frame.score = pin + pins[index.succ]+ pins[index.succ.succ]
-              skip_flag  = true
-            end
+      if index > 9
+          break
+      end
+      if pin == 10
 
-          elsif !pins[index.succ].nil? and  pin + pins[index.succ] > 10
-            raise "Bad Input."
-      else
+        if pins[index.succ].nil?
+             frame.score = pin
+        elsif pins[index.succ.succ].nil?
+             frame.score = pin + pins[index.succ]
+        else
+             frame.score = pin + pins[index.succ]+ pins[index.succ.succ]
+        end
+
+      elsif  !pins[index.succ].nil? and  pin + pins[index.succ] == 10
+
+        if pins[index.succ.succ].nil?
+            frame.score = pin + pins[index.succ]
+            skip_flag  = true
+        else
+            frame.score = pin + pins[index.succ]+ pins[index.succ.succ]
+            skip_flag  = true
+        end
+
+     elsif !pins[index.succ].nil? and  pin + pins[index.succ] > 10
+           raise "Bad Input."
+     else
       if pins[index.succ].nil?
-        frame.score = pin
-        skip_flag  = true
+         frame.score = pin
+         skip_flag  = true
       else
         frame.score = pin + pins[index.succ]
         skip_flag  = true
@@ -54,20 +57,20 @@ class Bowling
 
       end
     end
-    sum = 0
-    frames.each do |frame|
-      if !frame.score.nil?
+  sum = 0
+  frames.each do |frame|
+    if !frame.score.nil?
         sum +=frame.score
-      end
-
+     end
     end
    sum
   end
+
   def input_string=(str)
     @input_string = str
   end
 end
 
 
-test = Bowling.new("10 10 10 2 10 10 10 10 10 1")
+test = Bowling.new("10 10 10 2 , 0 10 10 10 10 1")
 puts test.count()
